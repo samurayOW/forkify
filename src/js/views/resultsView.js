ab5777dd570091c1429b615e5915cd3f4b7b6184
samurayOW <edicmichailov@gmail.com>
2024-08-01 16:23:32 +0300

    Initial commit

---
+++ src/js/views/resultsView.js
@@ -0,0 +1,17 @@
+import previewView from "./previewView.js";
+import View from "./View.js";
+import icons from 'url:../../img/icons.svg'  // Parcel 2
+
+class ResultsView extends View {
+  _parentElement = document.querySelector('.results');
+  _errorMeassage = 'No recepies found for your query! Please try again ;)';
+  _message = '';
+
+  _generateMarkup() {
+    return this._data
+      .map(result => previewView.render(result, false))
+      .join('');
+  }
+}
+
+export default new ResultsView();
